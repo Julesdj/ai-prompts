76c3662ce63eb8b4e57048aad57a1e06614cc11c
Kokutse Djoguenou <kokutse@Kokutses-MacBook-Pro.local>
2023-05-09 12:49:12 -0400

    Added search functionality

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -18,10 +18,12 @@ const PromptCardList = ({ data, handleTagClick }) => {
 };
 
 function Feed() {
+    // Search states
     const [searchText, setSearchText] = useState("");
-    const [posts, setPosts] = useState([]);
+    const [searchTimeout, setSearchTimeout] = useState(null);
+    const [searchedResults, setSearchedResults] = useState([]);
 
-    const handleSearchChange = (e) => {};
+    const [posts, setPosts] = useState([]);
 
     useEffect(() => {
         const getPosts = async () => {
@@ -34,19 +36,58 @@ function Feed() {
         getPosts();
     }, []);
 
+    // Regex Matching
+    const filterPrompts = (searchtext) => {
+        const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+        return posts.filter(
+            (item) =>
+                regex.test(item.creator.username) ||
+                regex.test(item.tag) ||
+                regex.test(item.prompt)
+        );
+    };
+
+    const handleSearchChange = (e) => {
+        clearTimeout(searchTimeout);
+        setSearchText(e.target.value);
+
+        // debounce method
+        setSearchTimeout(
+            setTimeout(() => {
+                const searchResult = filterPrompts(e.target.value);
+                setSearchedResults(searchResult);
+            }, 500)
+        );
+    };
+
+    // Click tag for result
+    const handleTagClick = (tagName) => {
+        setSearchText(tagName);
+
+        const searchResult = filterPrompts(tagName);
+        setSearchedResults(searchResult);
+    };
+
     return (
         <section className="feed">
             <form className=" relative w-full flex items-center">
                 <input
                     type="text"
-                    placeholder="Search for a tag or a username"
+                    placeholder="Search for matching words, tags or username"
                     value={searchText}
                     onChange={handleSearchChange}
                     required
                     className="search_input peer"
                 />
             </form>
-            <PromptCardList data={posts} handleTagClick={() => {}} />
+            {searchText ? (
+                <PromptCardList
+                    data={searchedResults}
+                    handleTagClick={handleTagClick}
+                />
+            ) : (
+                <PromptCardList data={posts} handleTagClick={handleTagClick} />
+            )}
         </section>
     );
 }
